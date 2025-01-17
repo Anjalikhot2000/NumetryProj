216d26113932e5f24dc90d94971ffa0031bb036f
Anjali Khot <anjalikhot5268@gmail.com>
2025-01-17 14:43:25 +0530

    Update server.js

--- react-signup-form/backend/server.js
+++ react-signup-form/backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
 
